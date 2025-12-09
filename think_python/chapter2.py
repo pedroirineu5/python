77f30391b2f01513d8af6f3cdc6b88a05bcfc0c6
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2025-12-08 21:35:21 -0300

    chapter3: functions (on going...)

---
+++ think_python/chapter2.py
@@ -0,0 +1,18 @@
+# Introdução a variaveis normais.
+n = 17
+pi = 3.14
+message = 'uma coisa diferente'
+
+# Ordem de precendência PEMDAS (POTENCIACAO -> DIVISÃO/MULTIPLICACAO -> ADICAO/SUBTRAÇÃO )
+print(32**2+2)
+
+
+# tentando printar uma atribuição para uma variável
+print(x=2)
+
+
+#sobre comentários é bom vc escolher um bom nome de variavel para que consigam entender o seu código, e faça o necessário para n ficar mt longo pois tornam essas expressões em expressões complexas.
+
+print('string' + 'outrastring')
+
+print('spam'*3 )
---
+++ think_python/charpter3.py
@@ -0,0 +1,18 @@
+### Functions
+
+print(type(42))
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+    print_lyrics()
+
+
+def print_lyrics():
+    print("I'm a lumberjack")
+    print("I sleep all night and I work all day")
+
+print(print_lyrics)
+print(type(print))
+
+repeat_lyrics()
